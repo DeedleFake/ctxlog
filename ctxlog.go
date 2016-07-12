d4a13091a264f1cd3a6da50f7237e17a9ce26a81
DeedleFake <deedlefake@users.noreply.github.com>
2016-07-12 15:44:08 -0400

    Change global functions into methods of a new Logger type.

--- ctxlog.go
+++ ctxlog.go
@@ -3,8 +3,10 @@ package ctxlog
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"strings"
+	"sync"
 )
 
 type contextKey int
@@ -13,16 +15,64 @@ const (
 	depthKey contextKey = iota
 )
 
-func depthFromContext(ctx context.Context) (depth int, ok bool) {
-	depth, ok = ctx.Value(depthKey).(int)
-	return
+// Depth returns the logging depth of a context.
+func Depth(ctx context.Context) int {
+	depth, _ := ctx.Value(depthKey).(int)
+	return depth
 }
 
-func Logf(ctx context.Context, format string, args ...interface{}) context.Context {
-	depth, _ := depthFromContext(ctx)
+type Logger struct {
+	once sync.Once
 
-	format = strings.TrimSpace(format)
-	fmt.Fprintf(os.Stderr, strings.Repeat("\t", depth)+format+"\n", args...)
+	output io.Writer
+	indent string
+}
+
+func (log *Logger) init() {
+	log.once.Do(func() {
+		log.output = os.Stderr
+		log.indent = "\t"
+	})
+}
+
+// SetOutput sets the output that the logger will write to. It
+// defaults to os.Stderr.
+func (log *Logger) SetOutput(output io.Writer) {
+	log.init()
+
+	log.output = output
+}
+
+// SetIndent sets the indent that will be prepended to log entries. It
+// will be multiplied by the depth of a log call's context. It
+// defaults to "\t".
+func (log *Logger) SetIndent(indent string) {
+	log.init()
+
+	log.indent = indent
+}
+
+// Log queues str to be printed when ctx ends. It returns a new
+// context with ctx as its parent that is deeper than ctx.
+func (log *Logger) Log(ctx context.Context, str string) context.Context {
+	log.init()
+
+	str = strings.TrimSpace(str)
+
+	depth := Depth(ctx)
+	fmt.Fprintln(output, strings.Repeat(indent, depth)+str)
+
+	return context.WithValue(ctx, depthKey, depth+1)
+}
+
+// Logf is like Log, but does Printf-style formatting.
+func (log *Logger) Logf(ctx context.Context, format string, args ...interface{}) context.Context {
+	log.init()
+
+	format = strings.TrimSpace(format) + "\n"
+
+	depth := Depth(ctx)
+	fmt.Fprintf(output, strings.Repeat(indent, depth)+format, args...)
 
 	return context.WithValue(ctx, depthKey, depth+1)
 }
